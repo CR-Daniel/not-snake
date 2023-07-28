7f66da7daf562c8f697c996207e0db47cdae9a74
CR-Daniel <d.chou.rainho@student.tue.nl>
2023-07-28 22:04:22 +0200

    hidden tail

--- main.js
+++ main.js
@@ -15,9 +15,10 @@ function initializeGame(options) {
   let snake = [[8,8]], apple = spawnApple(), [dx,dy] = [0,0];
   let score = 0;
   let highscore = localStorage.getItem('highscore') || 0;
-  let speed = options.shouldSpeedUp ? 250 : 125; // Adjust initial speed based on the shouldSpeedUp option
-  let minimumSpeed = 50; // Minimum limit for the speed
-  let tailSize = options.tailSize || 1; // Set initial tail size based on the tailSize option
+  let speed = options.shouldSpeedUp ? 250 : 125; 
+  let minimumSpeed = 50; 
+  let tailSize = options.tailSize || 1; 
+  let tailVisible = options.tailVisible !== undefined ? options.tailVisible : true;
 
   onkeydown = ({key}) => [dx,dy] =
     { [key]: [dx,dy], ArrowRight:[dx||1, 0], ArrowLeft:[dx||-1, 0] ,
@@ -30,23 +31,22 @@ function initializeGame(options) {
       case 'edges':
         let edge = Math.floor(Math.random() * 4);
         switch(edge) {
-          case 0: return [0, Math.floor(Math.random() * gridSize)]; // Left edge
-          case 1: return [gridSize-1, Math.floor(Math.random() * gridSize)]; // Right edge
-          case 2: return [Math.floor(Math.random() * gridSize), 0]; // Top edge
-          case 3: return [Math.floor(Math.random() * gridSize), gridSize-1]; // Bottom edge
+          case 0: return [0, Math.floor(Math.random() * gridSize)]; 
+          case 1: return [gridSize-1, Math.floor(Math.random() * gridSize)]; 
+          case 2: return [Math.floor(Math.random() * gridSize), 0]; 
+          case 3: return [Math.floor(Math.random() * gridSize), gridSize-1]; 
         }
         break;
       case 'corners':
         let corner = Math.floor(Math.random() * 4);
         switch(corner) {
-          case 0: return [0, 0]; // Top-left corner
-          case 1: return [gridSize-1, 0]; // Top-right corner
-          case 2: return [0, gridSize-1]; // Bottom-left corner
-          case 3: return [gridSize-1, gridSize-1]; // Bottom-right corner
+          case 0: return [0, 0]; 
+          case 1: return [gridSize-1, 0]; 
+          case 2: return [0, gridSize-1];
+          case 3: return [gridSize-1, gridSize-1];
         }
         break;
       default:
-        // Default to spawning apple randomly anywhere on the grid
         return [Math.floor(Math.random()*gridSize), Math.floor(Math.random()*gridSize)];
     }
   }
@@ -55,7 +55,6 @@ function initializeGame(options) {
     let nx = (snake[0][0] + dx);
     let ny = (snake[0][1] + dy);
 
-    // Check for boundary collision if bounded is set to true
     if (options.bounded && (nx < 0 || nx >= gridSize || ny < 0 || ny >= gridSize)) {
       resetGame();
       return;
@@ -67,16 +66,14 @@ function initializeGame(options) {
     }
 
     if (dx || dy) {
-      snake.unshift([nx, ny]); // add new head to snake
+      snake.unshift([nx, ny]); 
     }
 
     if ("" + [nx,ny] == apple) {
-      apple = spawnApple();  // Spawn a new apple
       do apple = spawnApple();
       while(snake.some(seg => ""+seg == apple));
       score++;
 
-      // Update tail size based on tailBehavior
       switch(options.tailBehavior) {
         case 'decreasing':
           tailSize = Math.max(1, tailSize - 1);
@@ -117,16 +114,22 @@ function initializeGame(options) {
     ctx.clearRect(0, 0, canvasSize, canvasSize);
     ctx.fillStyle = "red";
     ctx.fillRect(apple[0]*cellSize, apple[1]*cellSize, cellSize, cellSize);
-    ctx.fillStyle = "lime";
-    snake.forEach(([x,y]) => ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize));
+    
+    if(tailVisible) {
+      ctx.fillStyle = "lime";
+      snake.slice(1).forEach(([x,y]) => ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize)); // Draws the tail
+    }
+    
+    ctx.fillStyle = "lime"; // Color of the head
+    ctx.fillRect(snake[0][0]*cellSize, snake[0][1]*cellSize, cellSize, cellSize); // Draws the head
   }
 
   function resetGame() {
-    snake = [[8,8]]; // reset snake
-    apple = spawnApple(); // reset apple
-    score = 0; // reset score
-    tailSize = options.tailSize || 1; // reset tail size based on the tailSize option
-    [dx, dy] = [0, 0]; // reset direction
+    snake = [[8,8]]; 
+    apple = spawnApple(); 
+    score = 0; 
+    tailSize = options.tailSize || 1; 
+    [dx, dy] = [0, 0];
     document.getElementById("score").innerText = `Score: ${score}`;
     if(options.shouldSpeedUp) {
       speed = 250;
