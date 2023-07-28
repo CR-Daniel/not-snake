9cefb06b6c7f84424612ca72707f11a52274ce8b
CR-Daniel <d.chou.rainho@student.tue.nl>
2023-07-28 13:11:01 +0200

    diminish

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function initializeGame(options) {
   let highscore = localStorage.getItem('highscore') || 0;
   let speed = options.shouldSpeedUp ? 250 : 125; // Adjust initial speed based on the shouldSpeedUp option
   let minimumSpeed = 50; // Minimum limit for the speed
+  let tailSize = options.longTail ? 32 : 1; // Adjust initial tail length based on the longTail option
 
   onkeydown = ({key}) => [dx,dy] =
     { [key]: [dx,dy], ArrowRight:[dx||1, 0], ArrowLeft:[dx||-1, 0] ,
@@ -25,13 +26,19 @@ function initializeGame(options) {
   let game = setInterval(updateGame, speed);
 
   function updateGame() {
-    snake.unshift([ (snake[0][0] + dx) & 15 ,
-                    (snake[0][1] + dy) & 15 ]);
-    if(""+snake[0] == apple) {
+    let nx = (snake[0][0] + dx) & 15;
+    let ny = (snake[0][1] + dy) & 15;
+
+    if (dx || dy) {
+      snake.unshift([nx, ny]); // add new head to snake
+    }
+
+    if(""+[nx,ny] == apple) {
       do apple = [ Math.floor(Math.random()*gridSize) ,
                    Math.floor(Math.random()*gridSize) ];
       while(snake.some(seg => ""+seg == apple));
       score++;
+      if (options.longTail) tailSize = Math.max(1, tailSize - 1); // decrease tail size if longTail option is set
       document.getElementById("score").innerText = `Score: ${score}`;
       if (score > highscore) {
         highscore = score;
@@ -42,16 +49,18 @@ function initializeGame(options) {
       setTimeout(function() {
         document.getElementById('canvas').classList.remove('pulse');
       }, 1000);
-      
+
       if(options.shouldSpeedUp) {
         speed = Math.max(speed - 10, minimumSpeed);
         clearInterval(game);
         game = setInterval(updateGame, speed);
       }
     }
-    else if(snake.slice(1).some(seg => ""+seg == snake[0])) {
-      snake.splice(1);
-      score = 0;
+    else if (snake.slice(1).some(seg => ""+seg == [nx, ny])) {
+      snake = [[8,8]]; // reset snake
+      score = 0; // reset score
+      if (options.longTail) tailSize = 32; // reset tail size if longTail option is set
+      [dx, dy] = [0, 0]; // reset direction
       document.getElementById("score").innerText = `Score: ${score}`;
       if(options.shouldSpeedUp) {
         speed = 250;
@@ -59,8 +68,9 @@ function initializeGame(options) {
         game = setInterval(updateGame, speed);
       }
     }
-    else
+    while (snake.length > tailSize) {
       snake.pop();
+    }
     ctx.clearRect(0, 0, canvasSize, canvasSize);
     ctx.fillStyle = "red";
     ctx.fillRect(apple[0]*cellSize, apple[1]*cellSize, cellSize, cellSize);
