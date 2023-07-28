bec5442f577af1b81fb0d45f419750840ed673b6
CR-Daniel <d.chou.rainho@student.tue.nl>
2023-07-28 22:35:59 +0200

    toxic

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ function initializeGame(options) {
   `;
 
   let ctx = canvas.getContext("2d");
-  let snake = [[8,8]], apple = spawnApple(), [dx,dy] = [0,0];
+  let snake = [[8,8]], apple = spawnApple(), toxicApples = [], [dx,dy] = [0,0];
   let score = 0;
   let highscore = localStorage.getItem('highscore') || 0;
-  let speed = options.shouldSpeedUp ? 250 : 125; 
-  let minimumSpeed = 50; 
-  let tailSize = options.tailSize || 1; 
+  let speed = options.shouldSpeedUp ? 250 : 125;
+  let minimumSpeed = 50;
+  let tailSize = options.tailSize || 1;
   let tailVisible = options.tailVisible !== undefined ? options.tailVisible : true;
 
   onkeydown = ({key}) => [dx,dy] =
@@ -27,28 +27,45 @@ function initializeGame(options) {
   let game = setInterval(updateGame, speed);
 
   function spawnApple() {
-    switch(options.appleSpawn) {
-      case 'edges':
-        let edge = Math.floor(Math.random() * 4);
-        switch(edge) {
-          case 0: return [0, Math.floor(Math.random() * gridSize)]; 
-          case 1: return [gridSize-1, Math.floor(Math.random() * gridSize)]; 
-          case 2: return [Math.floor(Math.random() * gridSize), 0]; 
-          case 3: return [Math.floor(Math.random() * gridSize), gridSize-1]; 
-        }
-        break;
-      case 'corners':
-        let corner = Math.floor(Math.random() * 4);
-        switch(corner) {
-          case 0: return [0, 0]; 
-          case 1: return [gridSize-1, 0]; 
-          case 2: return [0, gridSize-1];
-          case 3: return [gridSize-1, gridSize-1];
-        }
-        break;
-      default:
-        return [Math.floor(Math.random()*gridSize), Math.floor(Math.random()*gridSize)];
+    let newApple;
+    do {
+      switch(options.appleSpawn) {
+        case 'edges':
+          let edge = Math.floor(Math.random() * 4);
+          switch(edge) {
+            case 0: newApple = [0, Math.floor(Math.random() * gridSize)]; break; // Left edge
+            case 1: newApple = [gridSize-1, Math.floor(Math.random() * gridSize)]; break; // Right edge
+            case 2: newApple = [Math.floor(Math.random() * gridSize), 0]; break; // Top edge
+            case 3: newApple = [Math.floor(Math.random() * gridSize), gridSize-1]; break; // Bottom edge
+          }
+          break;
+        case 'corners':
+          let corner = Math.floor(Math.random() * 4);
+          switch(corner) {
+            case 0: newApple = [0, 0]; break; // Top-left corner
+            case 1: newApple = [gridSize-1, 0]; break; // Top-right corner
+            case 2: newApple = [0, gridSize-1]; break; // Bottom-left corner
+            case 3: newApple = [gridSize-1, gridSize-1]; break; // Bottom-right corner
+          }
+          break;
+        default:
+          // Default to spawning apple randomly anywhere on the grid
+          newApple = [Math.floor(Math.random()*gridSize), Math.floor(Math.random()*gridSize)];
+      }
+    } while (snake.some(seg => ""+seg == newApple));
+    return newApple;
+  }
+
+  function spawnToxicApples() {
+    let newToxicApples = [];
+    for (let i = 0; i < (options.toxicApples || 0); i++) {
+      let newToxicApple;
+      do {
+        newToxicApple = [Math.floor(Math.random()*gridSize), Math.floor(Math.random()*gridSize)];
+      } while (snake.some(seg => ""+seg == newToxicApple));
+      newToxicApples.push(newToxicApple);
     }
+    return newToxicApples;
   }
 
   function updateGame() {
@@ -66,12 +83,12 @@ function initializeGame(options) {
     }
 
     if (dx || dy) {
-      snake.unshift([nx, ny]); 
+      snake.unshift([nx, ny]);
     }
 
     if ("" + [nx,ny] == apple) {
-      do apple = spawnApple();
-      while(snake.some(seg => ""+seg == apple));
+      apple = spawnApple();
+      toxicApples = spawnToxicApples();
       score++;
 
       switch(options.tailBehavior) {
@@ -103,38 +120,46 @@ function initializeGame(options) {
         clearInterval(game);
         game = setInterval(updateGame, speed);
       }
-    }
-    else if (snake.slice(1).some(seg => "" + seg == [nx, ny])) {
+    } else if (toxicApples.some(toxicApple => "" + [nx,ny] == toxicApple)) {
+      resetGame();
+      return;
+    } else if (snake.slice(1).some(seg => "" + seg == [nx, ny])) {
       resetGame();
       return;
     }
     while (snake.length > tailSize) {
       snake.pop();
     }
+
     ctx.clearRect(0, 0, canvasSize, canvasSize);
+
+    // Draw the apples
     ctx.fillStyle = "red";
     ctx.fillRect(apple[0]*cellSize, apple[1]*cellSize, cellSize, cellSize);
-    
+
+    // Draw toxic apples
+    ctx.fillStyle = "purple";
+    toxicApples.forEach(toxicApple => ctx.fillRect(toxicApple[0]*cellSize, toxicApple[1]*cellSize, cellSize, cellSize));
+
     if(tailVisible) {
       ctx.fillStyle = "lime";
-      snake.slice(1).forEach(([x,y]) => ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize)); // Draws the tail
+      snake.slice(1).forEach(([x,y]) => ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize));
     }
     
-    ctx.fillStyle = "lime"; // Color of the head
-    ctx.fillRect(snake[0][0]*cellSize, snake[0][1]*cellSize, cellSize, cellSize); // Draws the head
+    ctx.fillStyle = "lime";
+    ctx.fillRect(snake[0][0]*cellSize, snake[0][1]*cellSize, cellSize, cellSize);
   }
 
   function resetGame() {
-    snake = [[8,8]]; 
-    apple = spawnApple(); 
-    score = 0; 
-    tailSize = options.tailSize || 1; 
+    clearInterval(game);
+    snake = [[8,8]];
+    apple = spawnApple();
+    toxicApples = [];
     [dx, dy] = [0, 0];
+    score = 0;
+    speed = options.shouldSpeedUp ? 250 : 125;
+    tailSize = options.tailSize || 1;
     document.getElementById("score").innerText = `Score: ${score}`;
-    if(options.shouldSpeedUp) {
-      speed = 250;
-      clearInterval(game);
-      game = setInterval(updateGame, speed);
-    }
+    game = setInterval(updateGame, speed);
   }
 }
